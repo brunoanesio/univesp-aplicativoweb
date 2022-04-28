fc39b1fb6dab695791dfd911abac473e049e0802
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-04-28 10:02:37 -0300

    Código separado em arquivos para facilitar o desenvolvimento

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.exceptions import abort
-import datetime
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
 import os
-import sqlite3
+# import sqlite3
 import psycopg2
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
 # SQLite DB
+# project_dir = os.path.dirname(os.path.abspath(__file__))
 # database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 # def get_db_connection():
 #     conn = sqlite3.connect('database.db')
@@ -17,96 +25,31 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-
 conn = psycopg2.connect(uri, sslmode='require')
 
-app = Flask('__name__')
-app.config['SECRET_KEY'] = 'your secret key'
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
-app.config['SESSION_COOKIE_NAME'] = "my_session"
-db = SQLAlchemy(app)
+# Login
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    title = db.Column(db.String(80), nullable=False)
-    telefone = db.Column(db.String(11), nullable=False)
-    content = db.Column(db.String(200), nullable=False)
+def create_app():
+    app = Flask('__name__')
 
-    def __repr__(self):
-        return '<title %r' % self.id
+    app.config['SECRET_KEY'] = 'your secret key'
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    app.config['SESSION_COOKIE_NAME'] = "my_session"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
-@app.route('/')
-def index():
-    posts = Posts.query.all()
-    return render_template('index.html', posts=posts)
-
-
-def get_post(post_id):
-    post = Posts.query.filter_by(id=post_id).first()
-    if post is None:
-        abort(404)
-    return post
+    return app
 
 
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
-
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['create']
-        telefone = request.form['telefone']
-
-        if not title or not content or not telefone:
-            flash('É obrigatório preencher todas as informações!')
-        else:
-            post = Posts(title=title, content=content, telefone=telefone)
-            db.session.add(post)
-            db.session.commit()
-            return redirect(url_for('index'))
-
-    return render_template('create.html')
-
-
-@app.route('/edit/<int:id>', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['create']
-        telefone = request.form['telefone']
-
-        if not title or not content:
-            flash('É obrigatório preencher todas as informações!')
-        else:
-            post.title = title
-            post.content = content
-            post.telefone = telefone
-            db.session.commit()
-            return redirect(url_for('index'))
-
-    return render_template('edit.html', post=post)
-
-
-@app.route('/delete/<int:id>', methods=('GET', 'POST'))
-def delete(id):
-    post = get_post(id)
-
-    try:
-        db.session.delete(post)
-        db.session.commit()
-        return redirect(url_for('index'))
-    except:
-        return "Erro na hora de deletar o post."
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
